7950a04dafdaaf5138d147e1e15847fcd87bc148
KeelanJon <info@keelsdesign.co.uk>
2021-04-13 13:16:19 +0100

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,30 @@
 import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PrimaryButton from "../components/buttons/PrimaryButton"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Link to="/">
+        <PrimaryButton>Return to the Grid</PrimaryButton>
+      </Link>
+    </Container>
   </Layout>
 )
 
 export default NotFoundPage
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
