aac918030b23d72d3ca2c324757fcf08e07c00eb
KeelanJon <info@keelsdesign.co.uk>
2021-10-24 21:44:55 +0100

    Updated site menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -10,6 +10,7 @@ const MenuLinks = [
   { text: "Home", link: "/" },
   { text: "About", link: "/about" },
   { text: "Projects", link: "/#portfolioGrid" },
+  { text: "NFT Gallery", link: "/oddfolk" },
   { text: "Contact", link: "#contact" },
 ]
 
